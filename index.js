d686f161bb5bcd242b678c66d1db3e0d32859b9b
Auttawut <kzcofx@gmail.com>
2023-03-27 15:48:56 +0700

    delete music button

--- index.js
+++
@@ -1,8 +0,0 @@
-const audio = new Audio("./frosty-wind-nature-sounds-8051.mp3");
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    audio.play();
-  });
-});
